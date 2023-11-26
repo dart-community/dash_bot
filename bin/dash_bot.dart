3957b580eb2d870d2650d1b544e581e9adca5a65
Abitofevrything <mylo.fawcett@gmail.com>
2023-11-26 20:56:41 +0100

    Add "show-lint" command to show details about a linter rule

--- bin/dash_bot.dart
+++ bin/dash_bot.dart
@@ -1,3 +1,35 @@
-void main() {
-  print("Hi! 'I'm Dash!");
+import 'dart:io';
+
+import 'package:dash_bot/commands/show_lint.dart';
+import 'package:dash_bot/converters/linter_rules.dart';
+import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_commands/nyxx_commands.dart';
+import 'package:nyxx_extensions/nyxx_extensions.dart';
+
+void main() async {
+  final hasPrefix = Platform.environment.containsKey('PREFIX');
+
+  final commands = CommandsPlugin(
+    prefix: hasPrefix ? (_) => Platform.environment['PREFIX']! : null,
+  );
+
+  commands
+    ..addConverter(await createLinterRuleConverter())
+    ..addCommand(showLint);
+
+  Flags<GatewayIntents> intents = GatewayIntents.allUnprivileged;
+  if (hasPrefix) {
+    intents |= GatewayIntents.messageContent;
+  }
+
+  await Nyxx.connectGateway(
+    Platform.environment['TOKEN']!,
+    intents,
+    options: GatewayClientOptions(plugins: [
+      logging,
+      cliIntegration,
+      commands,
+      pagination,
+    ]),
+  );
 }
