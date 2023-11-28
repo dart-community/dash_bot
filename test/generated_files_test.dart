6459c5ba3f6d5d6c0ee3f2ea363d35c0924b7537
Abitofevrything <mylo.fawcett@gmail.com>
2023-11-28 23:22:59 +0100

    Add longer timeout for generated files test

--- test/generated_files_test.dart
+++ test/generated_files_test.dart
@@ -9,37 +9,41 @@ Stream<File> listGeneratedFilesInLib() => Directory('lib')
     .where((file) => file.path.endsWith('.g.dart'));
 
 void main() {
-  test('generated files are up to date', () async {
-    final originalContents = {
-      await for (final file in listGeneratedFilesInLib())
-        file.path: await file.readAsString(),
-    };
+  test(
+    'generated files are up to date',
+    timeout: Timeout.parse('10m'),
+    () async {
+      final originalContents = {
+        await for (final file in listGeneratedFilesInLib())
+          file.path: await file.readAsString(),
+      };
 
-    final buildResult = Process.runSync(Platform.resolvedExecutable,
-        ['run', 'build_runner', 'build', '--delete-conflicting-outputs']);
-    expect(buildResult.exitCode, 0);
+      final buildResult = Process.runSync(Platform.resolvedExecutable,
+          ['run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+      expect(buildResult.exitCode, 0);
 
-    final updatedContents = {
-      await for (final file in listGeneratedFilesInLib())
-        file.path: await file.readAsString(),
-    };
+      final updatedContents = {
+        await for (final file in listGeneratedFilesInLib())
+          file.path: await file.readAsString(),
+      };
 
-    for (final originalPath in originalContents.keys) {
-      // File was deleted by build
-      expect(updatedContents, contains(originalPath));
-    }
+      for (final originalPath in originalContents.keys) {
+        // File was deleted by build
+        expect(updatedContents, contains(originalPath));
+      }
 
-    for (final updatedPath in updatedContents.keys) {
-      // File was created by build
-      expect(originalContents, contains(updatedPath));
-    }
+      for (final updatedPath in updatedContents.keys) {
+        // File was created by build
+        expect(originalContents, contains(updatedPath));
+      }
 
-    for (final MapEntry(key: path, value: originalContent)
-        in originalContents.entries) {
-      final updatedContent = updatedContents[path]!;
+      for (final MapEntry(key: path, value: originalContent)
+          in originalContents.entries) {
+        final updatedContent = updatedContents[path]!;
 
-      // File was modified by build
-      expect(originalContent, equals(updatedContent));
-    }
-  });
+        // File was modified by build
+        expect(originalContent, equals(updatedContent));
+      }
+    },
+  );
 }
