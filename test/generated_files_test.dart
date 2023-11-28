c648293ac755d6c28933be382db85e8ab13339c9
Abitofevrything <mylo.fawcett@gmail.com>
2023-11-28 22:41:07 +0100

    Add test checking generated files are up to date

---
+++ test/generated_files_test.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+Stream<File> listGeneratedFilesInLib() => Directory('lib')
+    .list(recursive: true)
+    .where((entity) => entity is File)
+    .cast<File>()
+    .where((file) => file.path.endsWith('.g.dart'));
+
+void main() {
+  test('generated files are up to date', () async {
+    final originalContents = {
+      await for (final file in listGeneratedFilesInLib())
+        file.path: await file.readAsString(),
+    };
+
+    final buildResult = Process.runSync(Platform.resolvedExecutable,
+        ['run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+    expect(buildResult.exitCode, 0);
+
+    final updatedContents = {
+      await for (final file in listGeneratedFilesInLib())
+        file.path: await file.readAsString(),
+    };
+
+    for (final originalPath in originalContents.keys) {
+      // File was deleted by build
+      expect(updatedContents, contains(originalPath));
+    }
+
+    for (final updatedPath in updatedContents.keys) {
+      // File was created by build
+      expect(originalContents, contains(updatedPath));
+    }
+
+    for (final MapEntry(key: path, value: originalContent)
+        in originalContents.entries) {
+      final updatedContent = updatedContents[path]!;
+
+      // File was modified by build
+      expect(originalContent, equals(updatedContent));
+    }
+  });
+}
